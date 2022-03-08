fb36e06fc6da5dec8d7b2779aea9770591033be4
Behzad Qasim <86452588+behzadqasim@users.noreply.github.com>
2022-03-08 12:53:18 +0500

    Create Problem2.js

---
+++ Lab 2/Problem2.js
@@ -0,0 +1,7 @@
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 };
+console.log("Before Deleting object is: ",student);
+delete student.rollno;
+console.log("After Deleting object is: ",student);
